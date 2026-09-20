785c57f54817ef64d1fcff552cbdf7c9e93d5ae1
Ivan Espinola <94943628+ivanespinola10@users.noreply.github.com>
2026-09-20 18:43:36 -0300

    chore: remove redundant Flutter import

--- lib/gestor_archivos.dart
+++ lib/gestor_archivos.dart
@@ -1,7 +1,6 @@
 import 'package:file_picker/file_picker.dart';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 
 class CategoriaDinamica {
